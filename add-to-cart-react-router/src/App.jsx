19f1b6854ea527b9271340c41e894538a71ef54c
kaneshak79 <kaneshak79@gmail.com>
2026-01-23 14:56:50 +0530

    Error fixes

--- add-to-cart-react-router/src/App.jsx
+++ add-to-cart-react-router/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter,Routes,Route } from 'react-router-dom'
+import { HashRouter,Routes,Route, BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Products from './Pages/Products'
 import Cart from './Pages/cart'
@@ -9,14 +9,14 @@ function App() {
   return (
     <div>
       <CartProvider>
-      <HashRouter>
+      <BrowserRouter>
       <Navbar/>
       
       <Routes>
         <Route path="/" element={<Products/>} />
         <Route path="/cart" element={<Cart/>} />
       </Routes>
-      </HashRouter>
+      </BrowserRouter>
       </CartProvider>
     </div>
   )
